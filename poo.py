adcfbc7705acffbf2d721bf662fd649256efc7bc
jimenafig <110356677+jimenafig@users.noreply.github.com>
2022-07-31 15:41:37 -0600

    Add files via upload

---
+++ poo.py
@@ -0,0 +1,22 @@
+class medicinas:
+
+    def beneficios(a):
+        if a > 0:
+            print("Beneficios")
+            print("Restablece la salud")
+            print("Alivia el dolor")
+            print("Es esencial para la supervivencia")
+
+    def efectossecundarios(a):
+        if a > 0:
+            print("Efectos secundarios")
+            print("Somnolencia")
+            print("Molestias estomacales")
+            print("Reacciones Alérgicas")
+
+    def riesgos(a):
+        if a > 0:
+            print("Riesgos")
+            print("Reacciones adversas")
+            print("Afecta órganos vitales")
+            print("Puede causar la muerte")
---
+++ programa poo.py
@@ -0,0 +1,9 @@
+import poo
+nombremed=input("Ingrese el nombre de la medicina: ")
+a=int(input("Ingrese la cantidad en miligramos: "))
+
+print(nombremed)
+print(a)
+poo.beneficios(a)
+poo.efectossecundarios(a)
+poo.riesgos(a)
